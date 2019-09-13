19abc57c20a593e66c26c966b3a1ad14afe1f643
Maiia Bakhova <Mathemilda@users.noreply.github.com>
2019-09-12 17:39:01 -0700

    only code for Chapter 26
    Here is a source file without comments.

---
+++ Part_IV_AdvAn&ML/Spark_Ch_26_Py.py
@@ -0,0 +1,79 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ## Chapter 26: Classification
+
+# COMMAND ----------
+
+bInput = spark.read.format("parquet").load("/databricks-datasets/definitive-guide/data/binary-classification")\
+  .selectExpr("features", "cast(label as double) as label")
+
+# COMMAND ----------
+
+print(bInput.count())
+bInput.show(5, False)
+
+# COMMAND ----------
+
+from pyspark.ml.classification import LogisticRegression
+lr = LogisticRegression()
+print(lr.explainParams()) # see all parameters
+lrModel = lr.fit(bInput)
+
+# COMMAND ----------
+
+print(lrModel.coefficients)
+print(lrModel.intercept)
+
+# COMMAND ----------
+
+summary = lrModel.summary
+print(summary.areaUnderROC)
+summary.roc.show()
+summary.pr.show()
+
+# COMMAND ----------
+
+summary.objectiveHistory
+
+# COMMAND ----------
+
+from pyspark.ml.classification import DecisionTreeClassifier
+dt = DecisionTreeClassifier()
+print(dt.explainParams())
+dtModel = dt.fit(bInput)
+
+# COMMAND ----------
+
+from pyspark.ml.classification import RandomForestClassifier
+rfClassifier = RandomForestClassifier()
+print(rfClassifier.explainParams())
+trainedModel = rfClassifier.fit(bInput)
+
+# COMMAND ----------
+
+from pyspark.ml.classification import GBTClassifier
+gbtClassifier = GBTClassifier()
+print(gbtClassifier.explainParams())
+trainedModel = gbtClassifier.fit(bInput)
+
+# COMMAND ----------
+
+from pyspark.ml.classification import NaiveBayes
+nb = NaiveBayes()
+print(nb.explainParams())
+trainedModel = nb.fit(bInput.where("label != 0"))
+
+# COMMAND ----------
+
+from pyspark.mllib.evaluation import BinaryClassificationMetrics
+out = trainedModel.transform(bInput)\
+  .select("prediction", "label")\
+  .rdd.map(lambda x: (float(x[0]), float(x[1])))
+metrics = BinaryClassificationMetrics(out)
+
+# COMMAND ----------
+
+print(metrics.areaUnderPR)
+print(metrics.areaUnderROC)
+print("Receiver Operating Characteristic")
+metrics.roc.toDF().show()
